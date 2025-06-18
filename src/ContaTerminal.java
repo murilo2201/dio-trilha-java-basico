85a58107ecd088980f6e8010f6a8daabaffc4253
MuriloF <murilofs.franca@gmail.com>
2025-06-17 23:06:05 -0300

    feat: finalizado desafio Java basico com entrada via terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, por favor digite seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+
+        System.out.println("Perfeito! agora o número da sua agência:");
+        String agencia = scanner.nextLine();
+
+
+        System.out.println("Legal! Agora o número da conta, por favor:");
+        int numeroConta = scanner.nextInt();
+
+
+        System.out.println("Por último, informe seu saldo, por favor:");
+        double saldo = scanner.nextDouble();
+
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, " +
+                "sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numeroConta, saldo);
+
+        scanner.close();
     }
 }
